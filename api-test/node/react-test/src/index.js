b8082b4fb9e004b1a98210debf5e3a2f74362b18
ga111o <ga111o@proton.me>
2024-07-26 18:12:31 +0900

    📝 connect to server

--- api-test/node/react-test/src/index.js
+++ api-test/node/react-test/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./Login";
 import reportWebVitals from "./reportWebVitals";
+import LectureRequestForm from "./req";
+import UpdateUserInfo from "./updateUserInfo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Login />
+    <LectureRequestForm />
+    <UpdateUserInfo />
   </React.StrictMode>
 );
 
