7b30838df4851aa7b46a27c795391c8bc555c07a
ga111o <ga111o@proton.me>
2024-07-28 01:02:08 +0900

    ✨ insert selected lecture on db

--- api-test/node/react-test/src/index.js
+++ api-test/node/react-test/src/index.js
@@ -5,13 +5,15 @@ import Login from "./Login";
 import reportWebVitals from "./reportWebVitals";
 import LectureRequestForm from "./req";
 import UpdateUserInfo from "./updateUserInfo";
+import LectureSelection from "./req2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Login />
-    <LectureRequestForm />
+    {/* <LectureRequestForm /> */}
     <UpdateUserInfo />
+    <LectureSelection />
   </React.StrictMode>
 );
 
