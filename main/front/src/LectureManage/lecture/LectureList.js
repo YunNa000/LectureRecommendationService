c4344ec58457ed6c3fe5e6b171c16371f12daf49
ga111o <ga111o@proton.me>
2024-09-13 10:52:14 +0900

    📝 checkbox로 강의 선택

--- main/front/src/LectureManage/lecture/LectureList.js
+++ main/front/src/LectureManage/lecture/LectureList.js
@@ -6,7 +6,7 @@ import "./LectureList.css";
 const LectureList = ({ lectures, selectedLectures, setSelectedLectures }) => {
   const [user, setUser] = useState(null);
   const [expandedLectures, setExpandedLectures] = useState({});
-  const [visibleButtons, setVisibleButtons] = useState({}); // 새로운 상태 변수 추가
+  const [visibleButtons, setVisibleButtons] = useState({});
 
   const checkLoginStatus = async () => {
     const userId = Cookies.get("user_id");
@@ -177,14 +177,24 @@ const LectureList = ({ lectures, selectedLectures, setSelectedLectures }) => {
                       key={`${lecture.lectureID}-${index}`}
                       onClick={() => handleButtonVisibilityToggle(lectureKey)}
                     >
-                      <label>
+                      <label className="lecturelist-lecture-check-box">
+                        <input
+                          type="checkbox"
+                          checked={isSelected}
+                          onChange={() => handleLectureSelect(lecture)}
+                        />
                         <p className="lecturelist-lecture-moreinfo">
                           {lecture.moreInfo}
                         </p>
                         <div className="lecturelist-lecNameNstar-box">
                           <div className="lecturelist-lecName-box">
-                            {isSelected ? "✔️ " : ""}
-                            <p className="lecturelist-lecName">
+                            <p
+                              className={
+                                isSelected
+                                  ? "lecturelist-lecName-selected"
+                                  : "lecturelist-lecName"
+                              }
+                            >
                               {lecture.lecName}
                             </p>
                             <p className="lecturelist-lecProfessor">
@@ -193,39 +203,27 @@ const LectureList = ({ lectures, selectedLectures, setSelectedLectures }) => {
                           </div>
                           <div className="lecturelist-lecStar-box"></div>
                         </div>
-                        <div className="lecturelist-lec-detail">
-                          <p className="lecturelist-lecClassification">
-                            {lecture.lecClassification}
-                          </p>
-                          <p className="lecturelist-lecCredit">
-                            {lecture.lecCredit}학점
-                          </p>
-                          <p className="lecturelist-lecTime">
-                            {lecture.lecTime &&
-                            !lecture.lecTime !== "undefined" &&
-                            lecture.lecTime !== undefined ? (
-                              <>{formatLectureTime(lecture.lecTime)}</>
-                            ) : null}
-                          </p>
-                          <p className="lecturelist-lecClassroom">
-                            {lecture.lecClassroom}
-                          </p>
-                        </div>
-                        <div>
-                          에타정보도 받아와야해요. 백엔드에서 return하는 값에
-                          포함시켜야 해요.
-                        </div>
                       </label>
+                      <div className="lecturelist-lec-detail">
+                        <p className="lecturelist-lecClassification">
+                          {lecture.lecClassification}
+                        </p>
+                        <p className="lecturelist-lecCredit">
+                          {lecture.lecCredit}학점
+                        </p>
+                        <p className="lecturelist-lecTime">
+                          {lecture.lecTime &&
+                          lecture.lecTime !== "undefined" &&
+                          lecture.lecTime !== undefined ? (
+                            <>{formatLectureTime(lecture.lecTime)}</>
+                          ) : null}
+                        </p>
+                        <p className="lecturelist-lecClassroom">
+                          {lecture.lecClassroom}
+                        </p>
+                      </div>
                       {visibleButtons[lectureKey] && (
                         <div className="lecturelist-buttons">
-                          <button
-                            onClick={() => handleLectureSelect(lecture)}
-                            className="lecture-list-button-getlecture"
-                          >
-                            {selectedLectures[lectureKey]
-                              ? "강의 바구니에서 빼기"
-                              : "강의 바구니에 담기"}
-                          </button>
                           <button className="lecture-list-button-godetail">
                             강의 자세히 보기
                           </button>
@@ -247,21 +245,31 @@ const LectureList = ({ lectures, selectedLectures, setSelectedLectures }) => {
                   {expandedLectures[lecName] &&
                     lectureGroup.map((lecture, index) => {
                       const lectureKey = `${lecture.lecNumber}-${lecture.year}-${lecture.semester}`;
+                      const isSelected = !!selectedLectures[lectureKey];
                       return (
                         <div
                           key={`${lecture.lectureID}-${index}`}
-                          onClick={() =>
-                            handleButtonVisibilityToggle(lectureKey)
-                          }
                           className="lecturelist-samename-lectures"
                         >
                           <label>
+                            <input
+                              type="checkbox"
+                              checked={isSelected}
+                              onChange={() => handleLectureSelect(lecture)}
+                              className="lecturelist-lecture-check"
+                            />
                             <p className="lecturelist-lecture-moreinfo">
                               {lecture.moreInfo}
                             </p>
                             <div className="lecturelist-lecNameNstar-box">
                               <div className="lecturelist-lecName-box">
-                                <p className="lecturelist-lecName">
+                                <p
+                                  className={
+                                    isSelected
+                                      ? "lecturelist-lecName-selected"
+                                      : "lecturelist-lecName"
+                                  }
+                                >
                                   {lecture.lecName}
                                 </p>
                                 <p className="lecturelist-lecProfessor">
@@ -270,35 +278,30 @@ const LectureList = ({ lectures, selectedLectures, setSelectedLectures }) => {
                               </div>
                               <div className="lecturelist-lecStar-box"></div>
                             </div>
-                            <div className="lecturelist-lec-detail">
-                              <p className="lecturelist-lecClassification">
-                                {lecture.lecClassification}
-                              </p>
-                              <p className="lecturelist-lecCredit">
-                                {lecture.lecCredit}학점
-                              </p>
-                              <p className="lecturelist-lecTime">
-                                {lecture.lecTime &&
-                                !lecture.lecTime !== "undefined" &&
-                                lecture.lecTime !== undefined ? (
-                                  <>{formatLectureTime(lecture.lecTime)}</>
-                                ) : null}
-                              </p>
-                              <p className="lecturelist-lecClassroom">
-                                {lecture.lecClassroom}
-                              </p>
-                            </div>
                           </label>
+                          <div className="lecturelist-lec-detail">
+                            <p className="lecturelist-lecClassification">
+                              {lecture.lecClassification}
+                            </p>
+                            <p className="lecturelist-lecCredit">
+                              {lecture.lecCredit}학점
+                            </p>
+                            <p className="lecturelist-lecTime">
+                              {lecture.lecTime &&
+                              lecture.lecTime !== "undefined" &&
+                              lecture.lecTime !== undefined ? (
+                                <>{formatLectureTime(lecture.lecTime)}</>
+                              ) : null}
+                            </p>
+                            <p className="lecturelist-lecClassroom">
+                              {lecture.lecClassroom}
+                            </p>
+                          </div>
                           {visibleButtons[lectureKey] && (
                             <div className="lecturelist-buttons">
-                              <button
-                                onClick={() => handleLectureSelect(lecture)}
-                              >
-                                {selectedLectures[lectureKey]
-                                  ? "강의 취소"
-                                  : "강의 담기"}
+                              <button className="lecture-list-button-godetail">
+                                강의 자세히 보기
                               </button>
-                              <div>테스트용버튼</div>
                             </div>
                           )}
                         </div>
