50d05b53c94ac8e03a865c34cbbd40b6aa9ae8c1
ga111o <ga111o@proton.me>
2024-09-20 18:24:59 +0900

    📝 test용 chatgpt -> ollama

--- main/front/src/chatBot/ChatBot.js
+++ main/front/src/chatBot/ChatBot.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
-import chatbotImage from './chatbot.png';
-import { Send } from 'lucide-react';
-import './ChatBot.css';
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import chatbotImage from "./chatbot.png";
+import { Send } from "lucide-react";
+import "./ChatBot.css";
 
 const ChatBot = () => {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const [chatLog, setChatLog] = useState([]);
   const [selectedTheme, setSelectedTheme] = useState(null);
   const [availableThemes, setAvailableThemes] = useState([]);
@@ -16,39 +16,39 @@ const ChatBot = () => {
   }, []);
 
   const checkLoginStatus = async () => {
-    const userId = Cookies.get('user_id');
+    const userId = Cookies.get("user_id");
     try {
       if (userId) {
-        const response = await fetch('http://localhost:8000/', {
-          method: 'GET',
-          credentials: 'include',
+        const response = await fetch("http://localhost:8000/", {
+          method: "GET",
+          credentials: "include",
         });
         const data = await response.json();
         if (response.ok && data.user_id) {
-          console.log('User logged in:', data.user_id);
+          console.log("User logged in:", data.user_id);
         }
       } else {
-        console.log('로그인 해주세요.');
-        window.location.href = 'http://127.0.0.1:3000/login';
+        console.log("로그인 해주세요.");
+        window.location.href = "http://127.0.0.1:3000/login";
       }
     } catch (err) {
-      console.error('ChatBot.js - checkLogin', err);
+      console.error("ChatBot.js - checkLogin", err);
     }
   };
 
   const fetchAvailableThemes = async () => {
     try {
-      const response = await fetch('http://localhost:8000/themes/');
+      const response = await fetch("http://localhost:8000/themes/");
       const data = await response.json();
       setAvailableThemes(data.themes);
     } catch (err) {
-      console.error('Error fetching available themes:', err);
+      console.error("Error fetching available themes:", err);
     }
   };
 
   const formatText = (text) => {
-    if (typeof text !== 'string') return '';
-    return text.split('\n').map((line, index) => (
+    if (typeof text !== "string") return "";
+    return text.split("\n").map((line, index) => (
       <React.Fragment key={index}>
         {line}
         <br />
@@ -58,7 +58,7 @@ const ChatBot = () => {
 
   const getThemeContent = (theme) => {
     switch (theme) {
-      case '수강신청':
+      case "수강신청":
         return `<수강신청>에서는 아래의 내용에 대해 도움을 드릴 수 있습니다.
 
 • 수강신청 편성 
@@ -73,7 +73,7 @@ const ChatBot = () => {
 
 어떤 점이 궁금하세요?`;
 
-      case '졸업요건':
+      case "졸업요건":
         return `<졸업요건>에서는 아래의 내용에 대해 도움을 드릴 수 있습니다.
 
 • 입학년도 
@@ -82,7 +82,7 @@ const ChatBot = () => {
 • 다전공 여부
 
 어떤 점이 궁금하세요?`;
-      case '기타학사정보':
+      case "기타학사정보":
         return `<기타학사정보>에서는 아래의 내용에 대해 도움을 드릴 수 있습니다.
 
 • 교양필수교과목 
@@ -99,33 +99,33 @@ const ChatBot = () => {
 • 특별교육과정
 
 어떤 점이 궁금하세요?`;
-      case '교내전화번호안내':
+      case "교내전화번호안내":
         return `<교내전화번호안내>에서는 아래와 같이 적어주시면 알맞은 전화번호를 안내해드리도록 하겠습니다.
 
         • 예) 정보융합학부(정융) 과사무실 전화번호
         
         어떤 점이 궁금하세요?`;
       default:
-        return '';
+        return "";
     }
   };
 
   const handleThemeSelection = async (theme) => {
     setSelectedTheme(theme);
     try {
-      const response = await fetch('http://localhost:8000/selecttheme/', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8000/selecttheme/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ theme: theme }),
       });
       const data = await response.json();
       console.log(data.message);
       const themeContent = getThemeContent(theme);
-      setChatLog([{ type: 'system', text: themeContent }]);
+      setChatLog([{ type: "system", text: themeContent }]);
     } catch (err) {
-      console.error('Error selecting theme:', err);
+      console.error("Error selecting theme:", err);
     }
   };
 
@@ -134,55 +134,58 @@ const ChatBot = () => {
   };
 
   const handleSendMessage = async () => {
-    if (inputText.trim() === '') return;
+    if (inputText.trim() === "") return;
 
-    if (inputText.toLowerCase() === '/홈') {
+    if (inputText.toLowerCase() === "/홈") {
       setChatLog([]);
-      setInputText('');
+      setInputText("");
       setSelectedTheme(null);
+
       return;
     }
 
-    setChatLog([...chatLog, { type: 'user', text: inputText }]);
+    setChatLog([...chatLog, { type: "user", text: inputText }]);
 
     try {
-      const response = await fetch('http://localhost:8000/chatbot/', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8000/chatbot/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ theme: selectedTheme, question: inputText }),
       });
       const data = await response.json();
       setChatLog((prevLog) => [
         ...prevLog,
-        { type: 'system', text: data.answer },
+        { type: "system", text: data.answer },
       ]);
     } catch (err) {
-      console.error('Error asking question:', err);
+      console.error("Error asking question:", err);
       setChatLog((prevLog) => [
         ...prevLog,
-        { type: 'system', text: '죄송합니다. 오류가 발생했습니다.' },
+        { type: "system", text: "죄송합니다. 오류가 발생했습니다." },
       ]);
     }
 
-    setInputText('');
+    setInputText("");
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSendMessage();
     }
   };
 
   return (
     <div className="chatbot-container">
-      <div className="chatbot-header">
-      
-      </div>
+      <div className="chatbot-header"></div>
       <div className="chatbot-content">
         <div className="intro-section">
-          <img src={chatbotImage} alt="Chatbot" className="chatbot-avatar-small" />
+          <img
+            src={chatbotImage}
+            alt="Chatbot"
+            className="chatbot-avatar-small"
+          />
           <div className="intro-message">
             <p>
               수강신청자료집에 대한 모든 것을 알고 있어요😏
@@ -196,7 +199,9 @@ const ChatBot = () => {
             <button
               key={theme}
               onClick={() => handleThemeSelection(theme)}
-              className={`theme-button ${selectedTheme === theme ? 'selected' : ''}`}
+              className={`theme-button ${
+                selectedTheme === theme ? "selected" : ""
+              }`}
             >
               {theme}
             </button>
@@ -205,8 +210,12 @@ const ChatBot = () => {
         <div className="chat-log">
           {chatLog.map((entry, index) => (
             <div key={index} className={`chat-message ${entry.type}`}>
-              {entry.type === 'system' && (
-                <img src={chatbotImage} alt="Chatbot" className="chatbot-avatar-small" />
+              {entry.type === "system" && (
+                <img
+                  src={chatbotImage}
+                  alt="Chatbot"
+                  className="chatbot-avatar-small"
+                />
               )}
               <div className="message-content">{formatText(entry.text)}</div>
             </div>
--- main/server/router/chat.py
+++ main/server/router/chat.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from langchain.prompts import ChatPromptTemplate
 from langchain.embeddings import CacheBackedEmbeddings
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain.schema import Document
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
@@ -16,20 +16,23 @@ from langchain.chat_models import ChatOllama
 app = FastAPI()
 router = APIRouter()
 
-embeddings = OllamaEmbeddings()
+embeddings = OllamaEmbeddings(
+    model="llama3:8b"
+)
 
-llm = ChatOllama(
-    temperature=0.1, 
-    streaming=True, 
-    model="llama3:8b", 
-    )
+
+llm = ChatOllama(  # type:ignore
+    model="llama3:8b",
+    temperature=0.1,
+    streaming=True,
+)
 
 # embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 # llm = ChatOpenAI(
-#     temperature=0.1, 
-#     streaming=True, 
-#     model="gpt-3.5-turbo", 
+#     temperature=0.1,
+#     streaming=True,
+#     model="gpt-3.5-turbo",
 #     openai_api_key=OPENAI_API_KEY)
 
 
@@ -37,7 +40,7 @@ llm = ChatOllama(
 theme_retrievers = {}
 
 # MD 파일이 저장된 디렉토리 경로
-MD_DIR = r"D:/LectureRecommendationService/main\server/.cache\data_"
+MD_DIR = r"/home/ga111o/document/VSCode/kwu-lecture-recommendation-service/main/server/.cache/data_"
 
 # 테마별 MD 파일 매핑
 THEME_FILES = {
@@ -54,21 +57,25 @@ THEME_FILES = {
         "특별교육과정 (CDP, R.O.T.C., 글로벌인재트랙인증제).md", "편입생이 이수해야 할 교과목 및 학점.md",
         "학-석사 연계과정.md", "현장실습학기제 안내.md", "K-MOOC 교내 온라인강좌 수강 안내.md"
     ],
-    "교내전화번호안내": ["교내전화번호안내.md"]
+    "교내전화번호안내": ["교내전화번호안내.md"],
+    "test": ["test.md"]
 }
+
 
 class Question(BaseModel):
     theme: str
     question: str
 
+
 def embed_file(file_path: str):
     cache_dir = f"./.cache/embeddings/{os.path.basename(file_path)}"
     if os.path.exists(cache_dir):
         cache_store = LocalFileStore(cache_dir)
-        embeddings = OpenAIEmbeddings()
+        embeddings = OllamaEmbeddings()
         cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
             embeddings, cache_store)
-        docs = [Document(page_content=open(file_path, "r", encoding='utf-8').read())]
+        docs = [Document(page_content=open(
+            file_path, "r", encoding='utf-8').read())]
         vectorstore = FAISS.from_documents(docs, cached_embeddings)
         return vectorstore.as_retriever()
     else:
@@ -76,15 +83,17 @@ def embed_file(file_path: str):
             file_content = file.read()
         cache_store = LocalFileStore(cache_dir)
         docs = [Document(page_content=file_content)]
-        embeddings = OpenAIEmbeddings()
+        embeddings = OllamaEmbeddings()
         cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
             embeddings, cache_store)
         vectorstore = FAISS.from_documents(docs, cached_embeddings)
         return vectorstore.as_retriever()
 
+
 def format_docs(docs):
     combined_docs = "\n\n".join(doc.page_content for doc in docs)
     return combined_docs
+
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -102,43 +111,52 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+
 @router.get("/themes/")
 async def list_themes():
     return {"themes": list(THEME_FILES.keys())}
 
+
 class ThemeSelect(BaseModel):
     theme: str
+
 
 @router.post("/selecttheme/")
 async def select_theme(theme_select: ThemeSelect):
     try:
         theme = theme_select.theme
+
         print(f"Selecting theme: {theme}")
         if theme not in THEME_FILES:
             print(f"Theme not found: {theme}")
             raise HTTPException(status_code=404, detail="Theme not found")
-        
+
         if theme not in theme_retrievers:
             combined_content = ""
             for file_name in THEME_FILES[theme]:
+
                 file_path = os.path.join(MD_DIR, file_name)
                 try:
                     with open(file_path, "r", encoding='utf-8') as file:
                         combined_content += file.read() + "\n\n"
                 except FileNotFoundError:
                     print(f"File not found: {file_path}")
-                    raise HTTPException(status_code=500, detail=f"File not found: {file_name}")
+                    raise HTTPException(
+                        status_code=500, detail=f"File not found: {file_name}")
                 except Exception as e:
                     print(f"Error reading file {file_path}: {str(e)}")
-                    raise HTTPException(status_code=500, detail=f"Error reading file: {file_name}")
-            
+                    raise HTTPException(
+                        status_code=500, detail=f"Error reading file: {file_name}")
+
             cache_dir = f"./.cache/embeddings/{theme}"
             cache_store = LocalFileStore(cache_dir)
-            embeddings = OpenAIEmbeddings()
-            cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_store)
+            embeddings = OllamaEmbeddings()
+            cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+                embeddings, cache_store)
             docs = [Document(page_content=combined_content)]
             vectorstore = FAISS.from_documents(docs, cached_embeddings)
             theme_retrievers[theme] = vectorstore.as_retriever()
+            print(theme_retrievers)
 
         print(f"Theme selected successfully: {theme}")
         return {"message": f"{theme} 테마가 선택되었습니다."}
@@ -146,11 +164,17 @@ async def select_theme(theme_select: ThemeSelect):
         raise he
     except Exception as e:
         print(f"Unexpected error in select_theme: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"An unexpected error occurred: {str(e)}")
 
-@router.post("/chatbot/") 
+
+@router.post("/chatbot/")
 async def ask_question(question: Question):
     print(f"Received question: {question}")  # 로깅 추가
+    print("=====================================")
+    print(theme_retrievers)
+    print("=====================================")
+
     if question.theme not in theme_retrievers:
         print(f"Theme not selected: {question.theme}")  # 로깅 추가
         raise HTTPException(status_code=400, detail="Theme not selected")
@@ -171,11 +195,12 @@ async def ask_question(question: Question):
         print(f"Generated response: {response.content}")  # 로깅 추가
 
         if isinstance(response, dict):
-            return JSONResponse(content={"answer": response.content})
+            return JSONResponse(content={"answer": response})
         else:
             return JSONResponse(content={"answer": str(response.content)})
     except Exception as e:
         print(f"Error occurred: {str(e)}")  # 로깅 추가
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"An error occurred: {str(e)}")
 
 app.include_router(router)
